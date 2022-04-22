36f04324ef4ff95800a84d9ec5cbf1ab1d33fc57
Ahir Shiv <imahirshiv@gmail.com>
2022-04-22 17:33:00 +0530

    initial commit

---
+++ myjs.js
@@ -0,0 +1,16 @@
+// var num = 100;
+// var num2 = 200;
+// var result = num + num2;
+// console.log(result);
+// var result = num / num2;
+// console.log(result);
+
+// // var Name = prompt('Tell me your name');
+
+// // console.log(Name);
+
+// var Name = prompt('Tell me your name');
+// var Age = prompt('Tell me your age');
+// alert('Your Name is '+ Name);
+// alert('Your Age is '+ Age);
+
